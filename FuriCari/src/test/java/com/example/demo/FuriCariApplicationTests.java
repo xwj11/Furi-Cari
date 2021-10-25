8f2fd1089333690cadb7ca3d37d7b1cbd79dfdf5
pengin0805 <blue.190316@icloud.com>
2021-10-25 15:58:13 +0900

    プロジェクト新規作成

---
+++ FuriCari/src/test/java/com/example/demo/FuriCariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuriCariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
